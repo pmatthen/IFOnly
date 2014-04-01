d7cca5cd270ead358e3bfa15eb5b28bd3db70a0e
Gule Sheikh <gulesh@gmail.com>
2014-04-01 00:07:09 -0500

    started calculations

---
+++ IFOnly/MacroCalculationViewController.h
@@ -0,0 +1,13 @@
+//
+//  MacroCalculationViewController.h
+//  IFOnly
+//
+//  Created by gule on 2014-03-28.
+//  Copyright (c) 2014 Tablified Solutions. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MacroCalculationViewController : UIViewController
+
+@end
